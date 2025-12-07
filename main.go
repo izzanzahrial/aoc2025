e50161cfb27bc51ee65058af09c5c6ca5d8f0b84
izzan zahrial <izzanzahrial@outlook.com>
2025-12-07 14:06:36 +0700

    add day 4 part 1

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/izzanzahrial/aoc2025/day1"
 	"github.com/izzanzahrial/aoc2025/day2"
 	"github.com/izzanzahrial/aoc2025/day3"
+	"github.com/izzanzahrial/aoc2025/day4"
 )
 
 func main() {
@@ -42,4 +43,11 @@ func main() {
 	}
 
 	slog.Info(fmt.Sprintf("Day 3 Part 1 result: %d", day3Part1))
+
+	day4Part1, err := day4.Part1()
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	slog.Info(fmt.Sprintf("Day 4 Part 1 result: %d", day4Part1))
 }
